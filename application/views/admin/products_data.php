f897f5744c5a5d3715dc5f39fa3ce94f770711e2
rohmanahmad <rohmanmail@gmail.com>
2016-12-26 21:13:34 +0700

    admin

--- application/views/admin/products_data.php
+++ application/views/admin/products_data.php
@@ -1,9 +1,14 @@
 ﻿<!--MENU SECTION END-->
+<?php
+if(!$type){
+    $type = 'not-set';
+}
+?>
 <div class="content-wrapper">
   <div class="container">
     <div class="row pad-botm">
       <div class="col-md-12">
-        <h4 class="header-line">Artikel Penawaran</h4>
+        <h4 class="header-line"><?=(isset($label_title))?$label_title:'No Label'?></h4>
       </div>
     </div>
     <div class="row">
@@ -11,13 +16,18 @@
             <table class="table table-bordered">
                 <tr>
                     <th>#</th>
+                    <th>Created</th>
                     <th>Title</th>
                     <th>Author</th>
-                    <th><?=anchor('admin/blogoffers/new','<i class="fa fa-file-o"></i><span class="hidden-xs link">New</span>','class="btn btn-warning btn-sm"')?></th>
+                    <th><?=anchor('admin/'.$type.'/new','<i class="fa fa-file-o"></i><span class="hidden-xs link">New</span>','class="btn btn-warning btn-sm"')?></th>
                 </tr>
                 <?php 
             if(!isset($post)){
-                echo "This page was accessed incorrectly";
+                ?>
+                <tr>
+                    <th colspan="5"> Data Not Found </th>
+                </tr>
+                <?php
             }else{ //display the post
                 ?>
                 <?php
@@ -26,15 +36,16 @@
                     ?>
                     <tr>
                         <td><?=$no?></td>
+                        <td><?=$r->created?></td>
                         <td>
-                            <b><?=$r['post_title']?></b><hr>
+                            <b><?=$r->title?></b><hr>
                             <p><?php
-                                echo substr($r['post'],0,100);
-                                echo (strlen($r['post']) > 100)?'...':'';
+                                echo $r->content;
+                                echo (strlen($r->content) > 100)?'...':'';
                                 ?>
                             </p>
                         </td>
-                        <td><?=$r['author']?></td>
+                        <td><?=$r->author?></td>
                         <td>
                             <style>
                                 .btn{
@@ -46,9 +57,10 @@
                                 }
                             </style>
                             <?php
-                            echo anchor('home/offers/'.$r['post_slug'],'<i class="fa fa-eye"></i><span class="hidden-xs link">Preview</span>','class="btn btn-primary btn-xs" title="Preview"');
-                            echo anchor('admin/blogoffer/edit/'.$r['post_slug'],'<i class="fa fa-pencil-square-o"></i><span class="hidden-xs link">Edit</span>','class="btn btn-default btn-xs" title="Edit"');
-                            echo anchor('admin/blogoffer/trash/'.$r['post_slug'],'<i class="fa fa-trash"></i><span class="hidden-xs link">Delete</span>','class="btn btn-danger btn-xs" title="Trash"');
+                            
+                            echo anchor('home/'.$type.'/'.$r->slug,'<i class="fa fa-eye"></i><span class="hidden-xs link">Preview</span>','class="btn btn-primary btn-xs" title="Preview"');
+                            echo anchor('admin/'.$type.'/edit/'.$r->slug,'<i class="fa fa-pencil-square-o"></i><span class="hidden-xs link">Edit</span>','class="btn btn-default btn-xs" title="Edit"');
+                            echo anchor('admin/'.$type.'/trash/'.$r->slug,'<i class="fa fa-trash"></i><span class="hidden-xs link">Delete</span>','class="btn btn-danger btn-xs" title="Trash"');
                             ?>
                         </td>
                     </tr>
